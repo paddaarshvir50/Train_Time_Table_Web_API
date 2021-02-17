b5645f0372ad363f3f7340369b80773ca72933dd
Arshvir Singh <paddaarshvir50@gmail.com>
2021-02-17 19:56:40 +1300

    Add project files.

---
+++ DatabaseScripts/data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[TrainTimeTableRecord] ON
+INSERT INTO [dbo].[TrainTimeTableRecord] ([Id], [Service], [Destination], [Time]) VALUES (5, N'Southern Line', N'Britomart', N'2021-02-20 08:00:00')
+INSERT INTO [dbo].[TrainTimeTableRecord] ([Id], [Service], [Destination], [Time]) VALUES (6, N'Eastern Line', N'Manukau', N'2021-02-20 17:00:00')
+INSERT INTO [dbo].[TrainTimeTableRecord] ([Id], [Service], [Destination], [Time]) VALUES (7, N'Eastern Line', N'Britomart', N'2021-02-21 09:45:00')
+INSERT INTO [dbo].[TrainTimeTableRecord] ([Id], [Service], [Destination], [Time]) VALUES (8, N'Southern Line', N'Papakura', N'2021-02-21 13:45:00')
+SET IDENTITY_INSERT [dbo].[TrainTimeTableRecord] OFF
